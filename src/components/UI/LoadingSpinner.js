a2f29246b0a60d22a06c86b92faf9f671ce1e71c
Gameonn <jindal.ankit89@gmail.com>
2023-07-08 15:17:28 +0530

    Indent code

--- src/components/UI/LoadingSpinner.js
+++ src/components/UI/LoadingSpinner.js
@@ -1,7 +1,10 @@
 import classes from './LoadingSpinner.module.css';
 
 const LoadingSpinner = () => {
-  return <div className={classes.spinner}></div>;
+  	return (
+		<div className={classes.spinner}>
+		</div>
+	);
 }
 
 export default LoadingSpinner;
